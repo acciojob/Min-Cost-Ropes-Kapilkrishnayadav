65045ec15df5c4498cc10760b84d0e06f260456f
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-23 22:45:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort();
+	let sum=arr[0],n=arr.length,c=0;
+	for(i=1;i<n;i++)
+		{
+			sum=sum+arr[i];
+			c=c+sum;
+		}
+	return c;
   
 }
 
